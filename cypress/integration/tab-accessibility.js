1da5d9b48ca42248dd617761ef4e683d0db3018b
Mans <mans@finder.com>
2020-02-06 16:52:56 +1100

    test(demo): test

---
+++ cypress/integration/tab-accessibility.js
@@ -0,0 +1,16 @@
+describe('Implement keyboard accessibility', function() {
+    
+        it('should focus finder logo', () => {
+          cy.visit('/')
+          
+        })
+        it('should focus search box', () => {
+          
+        })
+        it('should focus magnifier logo', () => {
+         
+        })
+        it('should focus money nav link', () => {
+         
+        })
+    })
