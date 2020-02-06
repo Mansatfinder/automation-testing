32d5d93333829c4a4cc995577d56a64c6be60d9e
Mans <mans@finder.com>
2020-02-06 14:28:35 +1100

    test(commit): first commit

---
+++ cypress/integration/firstTest.js
@@ -0,0 +1,13 @@
+describe("Implement keyboard accessibility", function() {
+    
+        it(`should focus finder logo`, () => {
+          cy.visit('https://www.finder.com.au')
+          
+        })
+        it(`should focus next menu`, () => {
+          
+        })
+        it(`should focus next menu`, () => {
+         
+        })
+    })
